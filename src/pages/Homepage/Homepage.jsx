f09077b681c57aea13b6dd6f9fe4cec6463d1217
Akndju <davidd.akindoju@gmail.com>
2021-08-24 11:30:34 +0100

    Created makeOrder section

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Brief from "./components/Brief/Brief";
 import ScrollTopBtn from "../../components/ScrollTopBtn";
 import "./Homepage.scss";
+import MakeOrder from "./components/MakeOrder/MakeOrder";
 
 const Homepage = () => {
   const [isNotAtTop, setIsNotAtTop] = useState(false);
@@ -15,9 +16,10 @@ const Homepage = () => {
 
   return (
     <div className="homepageContainer">
+      {isNotAtTop && <ScrollTopBtn />}
       <Header />
       <Brief />
-      {isNotAtTop && <ScrollTopBtn />}
+      <MakeOrder />
     </div>
   );
 };
---
+++ src/pages/Homepage/components/MakeOrder/MakeOrder.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MakeOrder.scss";
+
+const MakeOrder = () => {
+  return (
+    <div className="makeOrder">
+      <h1>Get Your Meals Delivered Straightaway!</h1>
+      <h4>Chop Belle-Full Anywhere You Are.</h4>
+
+      <button>Order Now</button>
+    </div>
+  );
+};
+
+export default MakeOrder;
