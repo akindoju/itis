df979ea0fba79fc87957cb9a5ff949c609f457d1
Olusola Akindoju <davidd.akindoju@gmail.com>
2023-06-06 17:58:06 +0100

    Added search modal

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
   const [navBarActive, setNavBarActive] = useState(false);
   const [isAuthClicked, setIsAuthClicked] = useState(false);
   const [isCartClicked, setIsCartClicked] = useState(false);
-  const [isSearchClicked, setIsSearchClicked] = useState(true);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isNavBarOptionsBtnClicked, setIsNavBarOptionsBtnClicked] =
     useState(false);
@@ -40,65 +40,41 @@ const NavBar = () => {
   }, []);
 
   useEffect(() => {
-    if ((isAuthClicked && authModal) || (isCartClicked && cartModal)) {
+    if (
+      (isAuthClicked && authModal) ||
+      (isCartClicked && cartModal) ||
+      (isSearchClicked && searchModal)
+    ) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "scroll";
     }
-  }, [isAuthClicked, authModal, isCartClicked, cartModal]);
+  }, [
+    isAuthClicked,
+    authModal,
+    isCartClicked,
+    cartModal,
+    isSearchClicked,
+    searchModal,
+  ]);
 
   return (
     <div className={navBarActive ? "navBar active" : "navBar"}>
-      <ul className="navBar__options">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-
-        {location.pathname === "/" ? (
-          <li>
-            <Scroll to="ready-to-eat" smooth={true} duration={1000}>
-              Ready-to-Eat
-            </Scroll>
-          </li>
-        ) : null}
-
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-
       <div className="navBar__utilities">
-        <form
-          className={
-            navBarActive
-              ? "navBar__utilities--search search-active"
-              : "navBar__utilities--search"
-          }
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-          onClick={() => setIsSearchClicked(true)}
-        >
-          <input placeholder="Search meal" />
-          <button type="submit">
-            <svg
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <title>search1</title>
-              <path d="M9.516 14.016q1.875 0 3.188-1.313t1.313-3.188-1.313-3.188-3.188-1.313-3.188 1.313-1.313 3.188 1.313 3.188 3.188 1.313zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281q-1.781 1.547-4.219 1.547-2.719 0-4.617-1.875t-1.898-4.594 1.898-4.617 4.617-1.898 4.594 1.898 1.875 4.617q0 0.984-0.469 2.227t-1.078 1.992l0.281 0.281h0.797z"></path>
-            </svg>
-          </button>
-        </form>
-
         <div className="navBar__utilities--icons">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            onClick={() => setIsSearchClicked(true)}
+            ref={searchIconRef}
+          >
+            <title>search1</title>
+            <path d="M9.516 14.016q1.875 0 3.188-1.313t1.313-3.188-1.313-3.188-3.188-1.313-3.188 1.313-1.313 3.188 1.313 3.188 3.188 1.313zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281q-1.781 1.547-4.219 1.547-2.719 0-4.617-1.875t-1.898-4.594 1.898-4.617 4.617-1.898 4.594 1.898 1.875 4.617q0 0.984-0.469 2.227t-1.078 1.992l0.281 0.281h0.797z"></path>
+          </svg>
+
           <div
             onClick={() => setIsCartClicked(!isCartClicked)}
             className="navBar__utilities--icons-meals"
@@ -190,21 +166,43 @@ const NavBar = () => {
               </svg>
             )}
           </div>
-
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="navBar__utilities--icons-menu"
-            onClick={() => setIsNavBarOptionsBtnClicked(true)}
-          >
-            <title>menu</title>
-            <path d="M3 13h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 7h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 19h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1z"></path>
-          </svg>
         </div>
       </div>
+
+      <ul className="navBar__options">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        {location.pathname === "/" ? (
+          <li>
+            <Scroll to="ready-to-eat" smooth={true} duration={1000}>
+              Ready-to-Eat
+            </Scroll>
+          </li>
+        ) : null}
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        className="navBar__utilities--icons-menu"
+        onClick={() => setIsNavBarOptionsBtnClicked(true)}
+      >
+        <title>menu</title>
+        <path d="M3 13h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 7h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 19h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1z"></path>
+      </svg>
 
       {isAuthClicked && authModal
         ? createPortal(
