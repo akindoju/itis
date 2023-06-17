5bde7cbd9dee11213e2eb16eda838647957aa948
Olusola Akindoju <davidd.akindoju@gmail.com>
2023-06-17 20:41:48 +0100

    Addded authentication guard

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -6,14 +6,9 @@ import { login, register } from "../../Redux/slices/userSlice";
 import Loader from "../Loader/Loader";
 import * as Yup from "yup";
 import { Formik } from "formik";
-import { Form } from "react-router-dom";
 
 const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
   const wrapperRef = useRef(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [fullName, setFullName] = useState("");
   const [isLoginClicked, setIsLoginClicked] = useState(true);
   const [isRegisterClicked, setIsRegisterClicked] = useState(false);
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
@@ -26,20 +21,24 @@ const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
   const errorMsg = useSelector((state) => state.user.error);
 
   const loginValidation = Yup.object().shape({
-    email: Yup.string().email("Invalid Email").required("Email is required"),
+    email: Yup.string()
+      .email("Invalid Email")
+      .required("Email Address is required"),
     password: Yup.string()
-      .min(2, "Your message should be at least 2 characters")
-      .required("Your message is required"),
+      .min(6, "Your password should be at least 6 characters")
+      .required("Your password is required"),
   });
 
   const registerValidation = Yup.object().shape({
     fullName: Yup.string()
-      .min(2, "Name should be at least 2 characters")
-      .required("Name is required"),
-    email: Yup.string().email("Invalid Email").required("Email is required"),
+      .min(3, "Name should be at least 2 characters")
+      .required("Full Name is required"),
+    email: Yup.string()
+      .email("Invalid Email")
+      .required("Email Address is required"),
     password: Yup.string()
-      .min(2, "Your message should be at least 2 characters")
-      .required("Your message is required"),
+      .min(6, "Your password should be at least 6 characters")
+      .required("Your password is required"),
     confirmPassword: Yup.string().oneOf(
       [Yup.ref("password"), null],
       "Passwords must match"
@@ -86,17 +85,22 @@ const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
                 password: "",
               }}
               validationSchema={loginValidation}
-              onSubmit={async () => {
+              onSubmit={async (values, { resetForm }) => {
                 setIsLoginLoading(true);
-                await dispatch(
+                const response = await dispatch(
                   login({
-                    email,
-                    password,
+                    email: values.email,
+                    password: values.password,
                   })
                 );
 
-                setIsLoginLoading(false);
-                setIsAuthClicked(false);
+                if (response.payload.message === "success") {
+                  setIsLoginLoading(false);
+                  setIsAuthClicked(false);
+                  resetForm();
+                } else {
+                  setIsRegisterLoading(false);
+                }
               }}
             >
               {({
@@ -108,24 +112,27 @@ const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
                 errors,
               }) => {
                 return (
-                  <form
-                    onSubmit={handleSubmit}
-                    className="authentication__forms--login"
-                  >
-                    <div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="authentication__forms--formik">
                       <input
+                        name="email"
                         type="email"
-                        placeholder="Email"
+                        placeholder="Email Address"
                         onChange={handleChange}
                         onBlur={handleBlur}
                         value={values.email}
                       />
 
-                      <p>{errors.email && touched.email && errors.email}</p>
+                      <div>
+                        <span>
+                          {errors.email && touched.email && errors.email}
+                        </span>
+                      </div>
                     </div>
 
-                    <div>
+                    <div className="authentication__forms--formik">
                       <input
+                        name="password"
                         type="password"
                         placeholder="Password"
                         onChange={handleChange}
@@ -133,14 +140,26 @@ const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
                         value={values.password}
                       />
 
-                      <p>
-                        {errors.password && touched.password && errors.password}
-                      </p>
+                      <div>
+                        <span>
+                          {errors.password &&
+                            touched.password &&
+                            errors.password}
+                        </span>
+                      </div>
                     </div>
 
                     <p>Forgot Password?</p>
 
-                    <button disabled={isLoginLoading} type="submit">
+                    <button
+                      disabled={
+                        !isCheckboxChecked ||
+                        isLoginLoading ||
+                        !values.email ||
+                        !values.password
+                      }
+                      type="submit"
+                    >
                       {isLoginLoading ? <Loader /> : "Login"}
                     </button>
 
@@ -154,84 +173,152 @@ const AuthModal = ({ setIsAuthClicked, authIconRef }) => {
           ) : null}
 
           {isRegisterClicked ? (
-            <form
-              onSubmit={async (e) => {
-                e.preventDefault();
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={registerValidation}
+              onSubmit={async (values, { resetForm }) => {
                 setIsRegisterLoading(true);
                 const response = await dispatch(
                   register({
-                    fullName,
-                    email,
-                    password,
+                    fullName: values.fullName,
+                    email: values.email,
+                    password: values.password,
                   })
                 );
 
                 if (response.payload.message === "success") {
                   setIsRegisterLoading(false);
                   setIsAuthClicked(false);
+                  resetForm();
+                } else {
+                  setIsRegisterLoading(false);
                 }
               }}
-              className="authentication__forms--register"
             >
-              <input
-                type="text"
-                placeholder="Full Name"
-                value={fullName}
-                onChange={({ target }) => {
-                  setFullName(target.value);
-                }}
-              />
+              {({
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                touched,
+                values,
+                errors,
+              }) => {
+                return (
+                  <form
+                    onSubmit={handleSubmit}
+                    className="authentication__forms--register"
+                  >
+                    <div className="authentication__forms--formik">
+                      <input
+                        name="fullName"
+                        type="text"
+                        placeholder="Full Name"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.fullName}
+                      />
 
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={({ target }) => {
-                  setEmail(target.value);
-                }}
-              />
+                      <div>
+                        <span>
+                          {errors.fullName &&
+                            touched.fullName &&
+                            errors.fullName}
+                        </span>
+                      </div>
+                    </div>
 
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={({ target }) => {
-                  setPassword(target.value);
-                }}
-              />
+                    <div className="authentication__forms--formik">
+                      <input
+                        name="email"
+                        type="email"
+                        placeholder="Email Address"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
 
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={({ target }) => {
-                  setConfirmPassword(target.value);
-                }}
-              />
+                      <div>
+                        <span>
+                          {errors.email && touched.email && errors.email}
+                        </span>
+                      </div>
+                    </div>
 
-              <div className="authentication__forms--register-tc">
-                <input
-                  type="checkbox"
-                  id="terms and conditions"
-                  onClick={() => {
-                    setIsCheckboxChecked(!isCheckboxChecked);
-                  }}
-                />
-                <span htmlFor="terms and conditions">
-                  I agree to the Terms and Condtions
-                </span>
-              </div>
+                    <div className="authentication__forms--formik">
+                      <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
 
-              <button
-                type="submti"
-                disabled={!isCheckboxChecked || isRegisterLoading}
-                className={!isCheckboxChecked ? "notChecked" : null}
-              >
-                {isRegisterLoading ? <Loader /> : "Register"}
-              </button>
+                      <div>
+                        <span>
+                          {errors.password &&
+                            touched.password &&
+                            errors.password}
+                        </span>
+                      </div>
+                    </div>
 
-              <span className="authentication__forms--error">{errorMsg}</span>
-            </form>
+                    <div className="authentication__forms--formik">
+                      <input
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="Confirm Password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.confirmPassword}
+                      />
+
+                      <div>
+                        <span>
+                          {errors.confirmPassword &&
+                            touched.confirmPassword &&
+                            errors.confirmPassword}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="authentication__forms--register-tc">
+                      <input
+                        type="checkbox"
+                        id="terms and conditions"
+                        onClick={() => {
+                          setIsCheckboxChecked(!isCheckboxChecked);
+                        }}
+                      />
+                      <label htmlFor="terms and conditions">
+                        I agree to the Terms and Condtions
+                      </label>
+                    </div>
+
+                    <button
+                      type="submti"
+                      disabled={
+                        !isCheckboxChecked ||
+                        isRegisterLoading ||
+                        !values.email ||
+                        !values.password ||
+                        !values.fullName
+                      }
+                      className={!isCheckboxChecked ? "notChecked" : null}
+                    >
+                      {isRegisterLoading ? <Loader /> : "Register"}
+                    </button>
+
+                    <span className="authentication__forms--error">
+                      {errorMsg}
+                    </span>
+                  </form>
+                );
+              }}
+            </Formik>
           ) : null}
         </div>
       </div>
